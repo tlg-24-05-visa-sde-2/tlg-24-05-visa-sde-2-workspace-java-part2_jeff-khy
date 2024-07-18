f44b1e802a09ded45bf103438a8f47b9a7b45bbd
Jefferson Khy <jeffersonk@tlgcohort.com>
2024-07-18 15:39:58 -0500

    equals() testing in Employee, SalariedEmployee

--- Lab03.2/test/com/javatunes/personnel/SalariedEmployeeTest.java
+++ Lab03.2/test/com/javatunes/personnel/SalariedEmployeeTest.java
@@ -10,10 +10,34 @@ import static org.junit.Assert.*;
 public class SalariedEmployeeTest {
 
     private SalariedEmployee emp;
-
+    private SalariedEmployee emp2;
     @Before
     public void setUp() {
         emp = new SalariedEmployee("Logan", Date.valueOf("2020-10-01"), 1500.0);
+        emp2 = new SalariedEmployee("Logan", Date.valueOf("2020-10-01"), 1500.0);
+    }
+
+    @Test
+    public void equals_shouldReturnFalse_differentName_sameHireDate_sameSalary() {
+        emp2.setName("Jeremy");
+        assertNotEquals(emp, emp2);
+    }
+
+    @Test
+    public void equals_shouldReturnFalse_sameName_differentHireDate_sameSalary() {
+        emp2.setHireDate(Date.valueOf("2020-02-02"));
+        assertNotEquals(emp, emp2);
+    }
+
+    @Test
+    public void equals_shouldReturnFalse_sameName_sameHireDate_differentSalary() {
+        emp2.setSalary(1299.0);
+        assertNotEquals(emp, emp2);
+    }
+
+    @Test
+    public void equals_shouldReturnTrue_allProperitesSame() {
+        assertEquals(emp, emp2);
     }
 
     @Test
