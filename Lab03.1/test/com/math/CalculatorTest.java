74a062c6a750254e8cb04b0f2a9f3d1f3182e021
Jefferson Khy <jeffersonk@tlgcohort.com>
2024-07-18 13:25:54 -0500

    added lifecycle methods @Before, @After

--- Lab03.1/test/com/math/CalculatorTest.java
+++ Lab03.1/test/com/math/CalculatorTest.java
@@ -9,26 +9,49 @@
 package com.math;
 
 import static org.junit.Assert.*;
-import org.junit.Test;
+
+import org.junit.*;
 
 public class CalculatorTest {
+    private Calculator calc;
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("beforeClass");
+    }
+
+    @Before
+    public void setUp() {
+        System.out.println("setUp");
+        calc = new Calculator();
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("tearDown");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("afterClass");
+    }
 
     @Test
     public void testIsEven() {
-        Calculator calc = new Calculator();
+        System.out.println("testIsEven");
         assertTrue(calc.isEven(10));
         assertFalse(calc.isEven(11));
     }
 
     @Test
     public void testDivide() {
-        Calculator calculator = new Calculator();
-        assertEquals(2.5, calculator.divide(5, 2), .001);
+        System.out.println("testDivide");
+        assertEquals(2.5, calc.divide(5, 2), .001);
     }
 
     @Test
     public void testAdd() {
-        Calculator calc = new Calculator();
+        System.out.println("testAdd");
         assertEquals(5, calc.add(1, 4));  // expected, actual
     }
 }
