170d596908778cfa7e84d2360aa87debee838a9d
Jefferson Khy <jeffersonk@tlgcohort.com>
2024-07-26 15:32:28 -0500

    lambdas and method::references in Department

--- Lab03.2/src/com/javatunes/personnel/Department.java
+++ Lab03.2/src/com/javatunes/personnel/Department.java
@@ -25,15 +25,11 @@ public class Department {
     }
 
     public void listEmployees() {
-        for (Employee emp : employees) {
-            System.out.println(emp);
-        }
+       employees.forEach(System.out::println);
     }
 
     public void workEmployees() {
-        for (Employee emp : employees) {
-            emp.work();
-        }
+        employees.forEach(Employee::work);
     }
 
     /**
@@ -41,10 +37,7 @@ public class Department {
      * it will look similar to the workEmployees() method above
      */
     public void payEmployees() {
-        // DONE
-        for (Employee emp : employees) {
-            emp.pay();
-        }
+        employees.forEach(Employee::pay);
     }
 
     // helper method to add an Employee to the collection
